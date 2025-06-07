9fd6d2569a8e71b3567d383870d8768bed206bfe
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2025-06-07 19:13:04 +0000

    List group tut

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,23 @@
 import ListGroup from "./components/ListGroup";
 
 function App(){
+    let items = [
+      "Ghana",
+      "Nigeria",
+      "Kenya",
+      "South Africa",
+      "Uganda",
+      "Tanzania",
+      "Zimbabwe",
+      "Rwanda",
+    ];
+
+    const handleSelectItem = (item: string) => {
+      console.log(item);
+    }
 
   return( <div>
-   <ListGroup />
+   <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
   </div>);
 }
 
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,32 +1,33 @@
-function ListGroup() {
-  var items = [
-    "Ghana",
-    "Nigeria",
-    "Kenya",
-    "South Africa",
-    "Uganda",
-    "Tanzania",
-    "Zimbabwe",
-    "Rwanda",
-  ];
+import { useState } from "react";
 
-  items = [];
 
-  if (items.length === 0) {
-    return (
-      <>
-        <h1>List of Items</h1> 
-        <p>No items found</p>
-      </>
-    );
-  }
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+
+function ListGroup({items, heading, onSelectItem} : ListGroupProps) {
+
+
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List of Items</h1>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No Items found, add some</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            onClick={() => { setSelectedIndex(index);
+              onSelectItem(item);
+             }}
+          
+            className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
+          >
             {item}
           </li>
         ))}
